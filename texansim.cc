6c3124db94c565bf67f54390e8cefa64cf305fee
Greg <gchristian@oggi.tamu.edu>
2015-10-07 14:32:31 -0500

    Macro file requires as argv[1]

--- texansim.cc
+++ texansim.cc
@@ -20,14 +20,17 @@ namespace txs = texansim;
 
 int main(int argc, char** argv)
 {
-	// UI manager
-	G4UImanager* UI = G4UImanager::GetUIpointer();
+	// Check for macro file argument
+	if(argc < 2) {
+		G4cerr << "usage: texansim <run.mac>\n";
+		return 1;
+	}
 
 	// GDML parser
 	G4GDMLParser parser;
 	parser.Read("/home/gacgroup/gchristian/packages/simulation/texansim/build/detectors.gdml");
 
-	// construct the default run manager
+	/// Construct the default run manager
 #ifdef G4MULTITHREADED
  	std::auto_ptr<G4RunManager> runManager (new G4MTRunManager);
 #else
@@ -47,27 +50,11 @@ int main(int argc, char** argv)
 	/// - Initialize G4 kernel
 	runManager->Initialize();
 
-	// // get the pointer to the UI manager and set verbosities
-	// UI->ApplyCommand("/run/verbose 1");
-	// UI->ApplyCommand("/event/verbose 1");
-	// UI->ApplyCommand("/tracking/verbose 1");
-
-
-
-  // //read a macro file of commands
-  // G4UImanager* UI = G4UImanager::GetUIpointer();
+  /// - Read a macro file of commands
+	G4UImanager* UI = G4UImanager::GetUIpointer();
   G4String command  = "/control/execute ";
-  G4String fileName = "/home/gacgroup/gchristian/packages/simulation/texansim/build/run1.mac";
+  G4String fileName = argv[1];
 	UI->ApplyCommand(command+fileName); 
 
-
-	// // start a run
-	// int numberOfEvent = 3;
-	// runManager->BeamOn(numberOfEvent);
-
-
-	// UI->ApplyCommand("/control/manual");
-
-	// job termination
 	return 0;
 }
