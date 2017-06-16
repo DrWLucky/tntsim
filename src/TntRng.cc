a255719e0d16745cd40c786390afe770ec99f8e5
Greg <gchristian@tamu.edu>
2017-06-16 12:48:47 -0500

    Added error handler to RNG limits stuff, to check if max nuber of attemmpts has been reached (to avoid infinite loops).

--- src/TntRng.cc
+++ src/TntRng.cc
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <fstream>
 #include <sstream>
 #include <algorithm>
@@ -5,27 +6,78 @@
 #include "Randomize.hh"
 #include "TntRng.hh"
 
+#include <stdio.h>
+#include <execinfo.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+namespace { 
+
+int kMaxTries = 10000;
+
+
+void handler(int sig) {
+  void *array[10];
+  size_t size;
+
+  // get void*'s for all entries on the stack
+  size = backtrace(array, 10);
+
+  // print out all the frames to stderr
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
+
+void check_max_tries(G4int& n, const char* fct, G4double* low, G4double* high)
+{
+	if(n++ == kMaxTries) {
+		G4cerr << "WARNING in TntRng::" << fct << ":: Failed to generate RNG ";
+		if(low)  { G4cerr << "above " << *low; }
+		if(high) { if(low) { G4cerr << " and"; } G4cerr << " below " << *high; }
+		G4cerr << " after " << kMaxTries << " attempts. Enter 1 to abort program "
+					 << "(and print stack trace), or 0 to continue..." << G4endl;
+		G4int val;
+		G4cin >> val;
+		if(val != 0) { handler(0); exit(1); }
+	}
+}
+
+}
 
 // TNT RNG (BASE CLASS) //
 
 G4double TntRng::GenerateAbove(G4double low)
 {
+	G4int n = 0;
 	G4double value;
-	do { value = Generate(); } while (value < low);
+	do { 
+		value = Generate();
+		check_max_tries(n, "GenerateAbove", &low, 0);
+	} while (value < low);
 	return value;
 }
 
 G4double TntRng::GenerateBelow(G4double high)
 {
+	G4int n = 0;
 	G4double value;
-	do { value = Generate(); } while (value >= high);
+	do {
+		value = Generate();
+		check_max_tries(n, "GenerateBelow", 0, &high);
+	} while (value >= high);
 	return value;
 }
 
 G4double TntRng::GenerateBetween(G4double low, G4double high)
 {
+	G4int n = 0;
 	G4double value;
-	do { value = Generate(); } while (value < low || value >= high);
+	do { 
+		value = Generate();
+		check_max_tries(n, "GenerateBetween", &low, &high);
+	} while (value < low || value >= high);
 	return value;
 }
 
