e6a2c95fb8b327b841a169834d7aab57dcc8a5a0
Greg <gchristian@tamu.edu>
2017-06-23 07:14:15 -0500

    Added simple error handler message macros

---
+++ include/TntError.hh
@@ -0,0 +1,12 @@
+#ifndef TNT_ERROR_FILE_123
+#define TNT_ERROR_FILE_123
+
+/// Error message ///
+#define TNTERR G4cerr << "ERROR (" << __FILE__ << ", line " <<  __LINE__ << ")::"
+
+/// Warning message ///
+#define TNTWAR G4cerr << "WARNING (" << __FILE__ << ", line " <<  __LINE__ << ")::"
+
+
+
+#endif
